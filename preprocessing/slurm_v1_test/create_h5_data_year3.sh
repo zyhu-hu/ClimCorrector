b392ee973831c569377c99a9dec915dab01587df
Zeyuan Hu <57764895+zyhu-hu@users.noreply.github.com>
2024-09-10 17:08:32 -0400

    add 2more year slurm

---
+++ preprocessing/slurm_v1_test/create_h5_data_year3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+  
+#SBATCH -n 1 # TODO number of cpu
+#SBATCH -o out-%j
+#SBATCH -e eo-%j
+#SBATCH -p huce_ice
+#SBATCH --contiguous
+##SBATCH -x holy2a18106
+##SBATCH -x holy2b[05101-05108,05201-05208,05301-05308,07101-07108,09202-09208]
+#SBATCH --mail-type=END
+#SBATCH --mail-user=zeyuan_hu@fas.harvard.edu
+#SBATCH -J create_data3 # job name
+#SBATCH -t 120                         # job time in minutes
+#SBATCH --mem-per-cpu=180000 #MB
+#SBATCH --no-requeue
+
+
+cd /n/home00/zeyuanhu/ClimCorrector
+source activate /n/holylfs04/LABS/kuang_lab/Lab/kuanglfs/zeyuanhu/mamba_env/climcorr
+
+python preprocessing/create_h5_data_v1.py \
+    'conv_replay_amip_2iter1.cam.h1.1982-*.nc' \
+    --data_path '/n/holylfs04/LABS/kuang_lab/Lab/sweidman/cesm_output/conv_replay_amip_2iter1/atm/hist/' \
+    --save_path '/n/home00/zeyuanhu/scratch/climcorr_preprocessing/v1_test1/year3/' \
+    --start_idx 0 \
+    --stride_sample 1 
