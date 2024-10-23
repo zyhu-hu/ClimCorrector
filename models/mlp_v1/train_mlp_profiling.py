4db2b53b250f24aa5fd49095fa8c2df797d957f7
Zeyuan Hu <57764895+zyhu-hu@users.noreply.github.com>
2024-10-23 17:02:37 -0400

    fix tqdm error

--- models/mlp_v1/train_mlp_profiling.py
+++ models/mlp_v1/train_mlp_profiling.py
@@ -249,14 +249,14 @@ def main(cfg: DictConfig) -> float:
 
             total_iterations = len(train_loader)
             # Wrap train_loader with tqdm for a progress bar
-            train_loop = tqdm(train_loader, desc=f'Epoch {epoch+1}')
+            # train_loop = tqdm(train_loader, desc=f'Epoch {epoch+1}')
             current_step = 0
             # for iteration, (data_input, target) in enumerate(train_loop):
             for iteration in range(len(train_loader)):
                 # Use NVTX range for data loading
 
                 torch.cuda.nvtx.range_push(f"Data Loading Iteration {iteration+1}")
-                data_input, target = next(train_loop)  # Get the next batch
+                data_input, target = next(train_loader)  # Get the next batch
                 torch.cuda.nvtx.range_pop() # End of Data Loading
 
                 if cfg.early_stop_step > 0 and current_step > cfg.early_stop_step:
