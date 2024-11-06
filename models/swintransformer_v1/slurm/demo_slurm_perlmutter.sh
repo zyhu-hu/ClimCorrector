48ab755b530a7ec0f72a8b0b7a72d73d47dcd089
Zeyuan Hu <57764895+zyhu-hu@users.noreply.github.com>
2024-11-05 22:38:13 -0500

    add swin training script example

--- models/swintransformer_v1/slurm/demo_slurm_perlmutter.sh
+++ models/swintransformer_v1/slurm/demo_slurm_perlmutter.sh
@@ -3,39 +3,36 @@
 #SBATCH -C gpu
 #SBATCH -q debug 
 #SBATCH -t 00:30:00
-#SBATCH --ntasks-per-node 4
+#SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 32
-#SBATCH --gpus-per-node 4
-#SBATCH -n 4
+#SBATCH --gpus-per-node 1
+#SBATCH -n 1
 #SBATCH --image=nvcr.io/nvidia/modulus/modulus:24.01
 ##SBATCH --mail-user=zeyuanh@nvidia.com
 ##SBATCH --mail-type=ALL
 ##SBATCH --output=out_%j.out
 ##SBATCH --error=eo_%j.err
 
-cmd="python train_lstm8th.py --config-name=config \
+cmd="python train_swintransformer.py --config-name=config \
         climcorr_path='/global/homes/z/zeyuanhu/ClimCorrector/' \
-        input_mean='/global/homes/z/zeyuanhu/ClimCorrector/preprocessing/normalization/inputs/input_mean_v1_test1_10year_sub7.npy' \
-        input_std='/global/homes/z/zeyuanhu/ClimCorrector/preprocessing/normalization/inputs/input_std_v1_test1_10year_sub7.npy' \
-        target_mean='/global/homes/z/zeyuanhu/ClimCorrector/preprocessing/normalization/outputs/target_mean_v1_test1_10year_sub7.npy' \
-        target_std='/global/homes/z/zeyuanhu/ClimCorrector/preprocessing/normalization/outputs/target_std_v1_test1_10year_sub7.npy' \
-        train_dataset_path='/global/homes/z/zeyuanhu/scratch/climcorr/v1_test1/' \
-        val_dataset_path='/global/homes/z/zeyuanhu/scratch/climcorr/v1_test1/' \
+        train_dataset_path='/global/homes/z/zeyuanhu/scratch/climcorr/v1_test1_iter2_processed/' \
+        val_dataset_path='/global/homes/z/zeyuanhu/scratch/climcorr/v1_test1_iter2_processed/' \
         save_path='/global/homes/z/zeyuanhu/scratch/climcorr/saved_models/' \
-        expname='lstm_v1_test' \
-        batch_size=1024 \
+        expname='swin_v1_test' \
+        batch_size=16 \
         num_workers=16 \
-        epochs=16 \
+        epochs=1 \
+        early_stop_step=-1 \
         loss='huber' \
         save_top_ckpts=15 \
         learning_rate=0.0008 \
         clip_grad=False \
         clip_grad_norm=0.5 \
         logger='wandb' \
-        wandb.project='corr_lstm8th_v1' \
+        wandb.project='corr_swin_v1' \
         scheduler_name='cosine_warmup' \
         scheduler.cosine_warmup.T_0=16 \
         scheduler.cosine_warmup.eta_min=5.0e-6 "
 
-cd /global/homes/z/zeyuanhu/ClimCorrector/models/lstm_v1/
+cd /global/homes/z/zeyuanhu/ClimCorrector/models/swintransformer_v1/
 srun -n $SLURM_NTASKS shifter bash -c "source ddp_export.sh && $cmd"
