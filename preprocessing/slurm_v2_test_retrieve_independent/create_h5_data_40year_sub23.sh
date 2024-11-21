0656837e9fb8157cf85ada1907903fc9ccd5b1f4
Zeyuan Hu <57764895+zyhu-hu@users.noreply.github.com>
2024-11-21 00:43:50 -0500

    add a sub23 40year slurm

---
+++ preprocessing/slurm_v2_test_retrieve_independent/create_h5_data_40year_sub23.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+  
+#SBATCH -n 1 # TODO number of cpu
+#SBATCH -o out-%j
+#SBATCH -e eo-%j
+#SBATCH -p huce_ice
+#SBATCH --contiguous
+##SBATCH -x holy2a18106
+##SBATCH -x holy2b[05101-05108,05201-05208,05301-05308,07101-07108,09202-09208]
+#SBATCH --mail-type=END
+#SBATCH --mail-user=zeyuan_hu@fas.harvard.edu
+#SBATCH -J create_data2 # job name
+#SBATCH -t 600                         # job time in minutes
+#SBATCH --mem-per-cpu=180000 #MB
+#SBATCH --no-requeue
+
+
+cd /n/home00/zeyuanhu/ClimCorrector
+source activate /n/holylfs04/LABS/kuang_lab/Lab/kuanglfs/zeyuanhu/mamba_env/climcorr
+
+python preprocessing/create_h5_data_v2_retrieve_independent.py \
+    'spcam_replay_2iter*.cam.h1.*.nc' \
+    --data_path '/n/home04/sweidman/holylfs04/CESM215_out/Run/archive/spcam_replay_2iter/atm/hist/' \
+    --save_path '/n/home00/zeyuanhu/scratch/climcorr_preprocessing/v2_iter2_40year_sub23/' \
+    --start_idx 0 \
+    --stride_sample 23 
